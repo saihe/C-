1322699fff00ae246528690ec6715d66062fc434
saihe <saihe.kyon@gmail.com>
2015-08-09 21:01:38 +0900

    Battle追加。

---
+++ Battle/Battle/Interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+    class Interface
+    {
+        public int command;
+        public int stamina = 20;
+        public int charge  = 0;
+
+        public int battle(int c)
+        {
+            switch (c)
+            {
+                case 0:
+                    stamina -= 1;
+                    charge += 1;
+                    break;
+                case 1:
+                    stamina -= 1;
+                    charge += 1;
+                    break;
+                case 2:
+                    if (charge >= 3)
+                    {
+                        stamina -= 3;
+                        charge = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+            return c;
+        }
+    }
+
+
+    
+}
